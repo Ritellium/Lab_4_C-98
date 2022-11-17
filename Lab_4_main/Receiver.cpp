1c7644874a636a6e0e3e3b1ada42312ae2f783cd
Riterium Time <riteriumtime@gmail.com>
2022-11-18 00:21:36 +0300

    Fight with magic numbers complete (Code is still a nightmare)

--- Lab_4_main/Receiver.cpp
+++ Lab_4_main/Receiver.cpp
@@ -11,6 +11,13 @@
 
 #pragma warning (disable:4996)
 
+constexpr int StandartSTRSize = 50;
+constexpr int BigSTRSize = 150;
+constexpr int MaxSynchoTime = 50;
+constexpr int SmallSleepTime = 10;
+constexpr int EndTime = 250;
+
+
 /*
 Написать программу для передачи сообщений между процессами через общий файл. 
 Программа включает один процесс Receiver и несколько процессов Sender. 
@@ -25,9 +32,9 @@ int main(int argc, char* argv[])
 	FILE* file_bin;
 	// Input for Creator
 
-	LPSTR file_name = new char[40];
+	LPSTR file_name = new char[StandartSTRSize];
 	printf("input name of binary file (it will be created): ");
-	scanf_s("%s", file_name, 41);
+	scanf_s("%s", file_name, StandartSTRSize);
 
 	error_file = fopen_s(&file_bin, file_name, "wb");
 	if (error_file != 0)
@@ -58,16 +65,16 @@ int main(int argc, char* argv[])
 	// Events initialization 
 	for (int i = 0; i < sendersEmount; i++)
 	{
-		char senderNum[10];
+		LPSTR senderNum = new char[StandartSTRSize];
 		itoa(i + 1, senderNum, 10);
 
-		names_senders_ready[i] = new char[40];
+		names_senders_ready[i] = new char[StandartSTRSize];
 		strcpy(names_senders_ready[i], "sender_ready_");
 		strcat(names_senders_ready[i], senderNum);
-		names_continue_work[i] = new char[40];
+		names_continue_work[i] = new char[StandartSTRSize];
 		strcpy(names_continue_work[i], "continue_work_");
 		strcat(names_continue_work[i], senderNum);
-		names_end_work[i] = new char[40];
+		names_end_work[i] = new char[StandartSTRSize];
 		strcpy(names_end_work[i], "end_work_");
 		strcat(names_end_work[i], senderNum);
 
@@ -80,19 +87,53 @@ int main(int argc, char* argv[])
 	string_counter = CreateSemaphoreA(nullptr, 0, stringsEmount, name_str_counter);
 	senders_counter = CreateSemaphoreA(nullptr, 0, sendersEmount, name_sender_counter);
 
+	if (senders_counter == nullptr || string_counter == nullptr)
+	{
+		printf("Wrong HANDLEs of sync objects");
+		// Чистка
+		{
+			delete[] names_end_work;
+			delete[] names_continue_work;
+			delete[] names_senders_ready;
+
+			delete[] end_work;
+			delete[] continue_work;
+			delete[] senders_ready;
+		}
+		return 0;
+	}
+	for (int i = 0; i < sendersEmount; i++)
+	{
+		if (senders_ready[i] == nullptr || continue_work[i] == nullptr || end_work[i] == nullptr)
+		{
+			printf("Wrong HANDLEs of sync objects");
+			// Чистка
+			{
+				delete[] names_end_work;
+				delete[] names_continue_work;
+				delete[] names_senders_ready;
+
+				delete[] end_work;
+				delete[] continue_work;
+				delete[] senders_ready;
+			}
+			return 0;
+		}
+	}
+
 	ReleaseSemaphore(string_counter, stringsEmount, nullptr);
 
 	// Senders work
-	LPSTR SenderAllocation = new char[50];
+	LPSTR SenderAllocation = new char[StandartSTRSize];
 	strcpy(SenderAllocation, "Sender.exe");
 
 	LPSTR* data = new LPSTR[sendersEmount];
 	for (int i = 0; i < sendersEmount; i++)
 	{
-		char senderNum[10];
+		LPSTR senderNum = new char[StandartSTRSize];
 		itoa(i + 1, senderNum, 10);
 
-		data[i] = new char[150];
+		data[i] = new char[BigSTRSize];
 		strcpy(data[i], SenderAllocation); // argv[1]
 		strcat(data[i], " ");
 		strcat(data[i], file_name); // argv[1]
@@ -163,7 +204,7 @@ int main(int argc, char* argv[])
 			if (end_of_threads[i] == 1)
 			{
 				WaitForSingleObject(senders_ready[i], INFINITE);
-				if (WaitForSingleObject(end_work[i], 50) != WAIT_TIMEOUT)
+				if (WaitForSingleObject(end_work[i], SmallSleepTime) != WAIT_TIMEOUT)
 				{
 					end_of_threads[i] = 0;
 					CloseHandle(senders_PrInf[i].hThread);
@@ -179,10 +220,10 @@ int main(int argc, char* argv[])
 		{
 			fopen_s(&file_bin, file_name, "rb");
 
-			char* readed_message = new char[30];
+			char* readed_message = new char[StandartSTRSize];
 			printf("Messages: \n");
 
-			while(WaitForSingleObject(senders_counter, 20) != WAIT_TIMEOUT)
+			while(WaitForSingleObject(senders_counter, SmallSleepTime) != WAIT_TIMEOUT)
 			{
 				MyFunctions::readMessage(file_bin, readed_message);
 				printf("%s\n", readed_message);
@@ -206,9 +247,9 @@ int main(int argc, char* argv[])
 				if (end_of_threads[i] == 1)
 				{
 					SetEvent(continue_work[i]);
-					Sleep(5);
+					Sleep(SmallSleepTime);
 					SetEvent(end_work[i]);
-					Sleep(25);
+					Sleep(SmallSleepTime);
 					CloseHandle(senders_PrInf[i].hThread);
 					CloseHandle(senders_PrInf[i].hProcess);
 				}
@@ -220,7 +261,7 @@ int main(int argc, char* argv[])
 		fclose(file_bin);
 
 		printf("Now waiting for messages\n");
-		Sleep(200);
+		Sleep(EndTime);
 	}
 	// Чистка
 	{
--- Sender/Sender.cpp
+++ Sender/Sender.cpp
@@ -7,6 +7,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+constexpr int StandartSTRSize = 21;
+constexpr int SmallSleepTime = 10;
+
 #pragma warning (disable:4996)
 
 int main(int argc, char* argv[])
@@ -14,13 +17,14 @@ int main(int argc, char* argv[])
     FILE* file_bin;
     errno_t error_file;
     const char* file_name = argv[1];
-    printf("I'm Sender number %s, type below to sent messages\n", argv[7]);
 
     LPCSTR name_senders_ready = argv[2];
     LPCSTR name_continue_work = argv[3];
     LPCSTR name_end_work = argv[4];
     LPCSTR string_counter = argv[5];
     LPCSTR senders_counter = argv[6];
+
+    printf("I'm Sender number %s, type below to sent messages\n", argv[7]);
 
     HANDLE this_ready = OpenEventA(EVENT_ALL_ACCESS, EVENT_MODIFY_STATE, name_senders_ready);
     HANDLE continue_work = OpenEventA(EVENT_ALL_ACCESS, EVENT_MODIFY_STATE, name_continue_work);
@@ -49,18 +53,18 @@ int main(int argc, char* argv[])
                 printf("Error opening file");
                 return 0;
             }
-            auto message = new char[21];
+            auto message = new char[StandartSTRSize];
             printf("Enter the message (maximum 20 symbols)\n");
-            scanf_s("%s", message, 20);
+            scanf_s("%s", message, StandartSTRSize);
 
-            if (WaitForSingleObject(semaphore_string, 50) == WAIT_TIMEOUT)
+            if (WaitForSingleObject(semaphore_string, SmallSleepTime) == WAIT_TIMEOUT)
             {
                 delete[] message;
                 printf("The file is full\n");
                 SetEvent(this_ready);
                 fclose(file_bin);
                 WaitForSingleObject(continue_work, INFINITE);
-                if (WaitForSingleObject(end_work, 50) == WAIT_TIMEOUT)
+                if (WaitForSingleObject(end_work, SmallSleepTime) == WAIT_TIMEOUT)
                 {
                     continue;
                 }
@@ -88,6 +92,5 @@ int main(int argc, char* argv[])
         }
     } while (true);
 
-    Sleep(500);
     return 0;
 }
